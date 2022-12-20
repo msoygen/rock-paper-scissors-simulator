12ec5848e3007aaf93dba36f20032e8d9e036e92
M Serdar Oygen <mserdaroygen@gmail.com>
2022-12-21 00:21:35 +0300

    update UI and game manager
    * Game Scene: put game stats parent closer to the top
    * Main Menu Scene: set min slider value to 3
    * Game Manager: pressing down ESC sets time scale to zero

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene(0);
         }
