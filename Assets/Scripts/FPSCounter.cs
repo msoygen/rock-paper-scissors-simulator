df9e6c64db3f25a4392726a988de4ef6cf655f72
msoygen <mserdaroygen@gmail.com>
2023-10-14 00:57:14 +0300

    *update: order, visibility

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -11,33 +11,25 @@ public class FPSCounter : MonoBehaviour
     float timeleft;
     float fps;
 
-    GUIStyle textStyle = new GUIStyle();
-
-    // Use this for initialization
     void Start()
     {
         timeleft = updateInterval;
-
-        textStyle.fontStyle = FontStyle.Bold;
-        textStyle.normal.textColor = Color.white;
     }
 
-    // Update is called once per frame
     void Update()
     {
         timeleft -= Time.deltaTime;
         accum += Time.timeScale / Time.deltaTime;
         ++frames;
 
-        // Interval ended - update GUI text and start new interval
         if (timeleft <= 0.0)
         {
             // display two fractional digits (f2 format)
-            countText.SetText(fps.ToString("F2") + " FPS");
             fps = (accum / frames);
             timeleft = updateInterval;
             accum = 0.0f;
             frames = 0;
+            countText.SetText(fps.ToString("F0") + " FPS");
         }
     }
 }
